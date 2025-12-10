38f421a9baee1a93f2355b962ffe01dc1237c5de
nbogie <neillbogie@googlemail.com>
2025-12-10 23:39:54 +0000

    Fix comment syntax for TypeScript checking

--- types/global-millefeuille.d.ts
+++ types/global-millefeuille.d.ts
@@ -1,4 +1,4 @@
-// @ts-check
+//@ts-check
 import {LayerSystem} from "./millefeuille.d.ts"
 
 //The createLayerSystem is created dynamically at runtime (in the global scope).  TypeScript didn't create a type for it in the d.ts
