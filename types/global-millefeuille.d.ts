e660232afd75fd732ca9d64007ac37e944839a72
nbogie <neillbogie@googlemail.com>
2025-12-10 23:23:25 +0000

    comment global-millefeuille.d.ts

--- types/global-millefeuille.d.ts
+++ types/global-millefeuille.d.ts
@@ -1,7 +1,10 @@
 // @ts-check
 import {LayerSystem} from "./millefeuille.d.ts"
 
+//The createLayerSystem is created dynamically at runtime (in the global scope).  TypeScript didn't create a type for it in the d.ts
+//We create one here, in the global scope, to match what will be available there at runtime.
 declare global {
+    
     function createLayerSystem(options?: object): LayerSystem;
     // type CreateLayerSystemResult = ReturnType<typeof createLayerSystem>;
 }
